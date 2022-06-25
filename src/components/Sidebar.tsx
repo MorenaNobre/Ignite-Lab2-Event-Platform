e05c89a10cd60c8a0e0d157239c8d892f2de35c5
MorenaNobre <morenagnobre@gmail.com>
2022-06-25 15:09:41 -0300

    Subscribe by mutation from GraphCMS / Button Logout

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 import { Lesson } from "./Lesson";
 
 const GET_LESSONS_QUERY = gql`
@@ -31,18 +32,28 @@ export function Sidebar() {
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
         Cronograma de aulas
       </span>
-      <div className="flex flex-col gap-8">
-        {data?.lessons.map((lesson) => {
-          return (
-            <Lesson
-              key={lesson.id}
-              title={lesson.title}
-              slug={lesson.slug}
-              availableAt={new Date(lesson.availableAt)}
-              type={lesson.lessonType}
-            />
-          );
-        })}
+      <div className="flex flex-col justify-between gap-12">
+        <div className="flex flex-col gap-8">
+          {data?.lessons.map((lesson) => {
+            return (
+              <Lesson
+                key={lesson.id}
+                title={lesson.title}
+                slug={lesson.slug}
+                availableAt={new Date(lesson.availableAt)}
+                type={lesson.lessonType}
+              />
+            );
+          })}
+        </div>
+        <div>
+          <Link
+            to={`/`}
+            className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
+          >
+            Sair
+          </Link>
+        </div>
       </div>
     </aside>
   );
